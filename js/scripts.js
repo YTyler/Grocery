f8c1a1d21fe6eaa07c1f6b2752d506051c8bf7d4
Student <example@example.com>
2019-10-23 09:36:15 -0700

    complete madlib project

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+    var person3Input = $("input#person3").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+    $(".person3").text(person3Input);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
