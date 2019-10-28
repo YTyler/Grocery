a3b6879f38e57c06639e7c0cc3af70bdd18b8cb1
Student <example@example.com>
2019-10-28 11:31:06 -0700

    create grocery repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function()  {
   $("#formOne").submit(function() {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-    var person3Input = $("input#person3").val();
+    var blanks = ['person1', 'person2', 'person3', 'animal', 'exclamation', 'verb', 'noun']
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    // var person3Input = $("input#person3").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-    $(".person3").text(person3Input);
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
+    // $(".person3").text(person3Input);
 
     $("#story").show();
 
